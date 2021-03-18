0aa819113feb0adefc55c562dfb2e8cc3c314c4b
Hector Romero <hectorromerodev@gmail.com>
2021-03-18 01:17:50 -0700

    UPD App js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const hamburgerBtn = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list')
+
+function toggleBtn() {
+  navList.classList.toggle('show');
+}
+
+hamburgerBtn.addEventListener('click', toggleBtn);
